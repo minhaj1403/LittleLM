442df90ce2f829eb8cb81cbc495ac7fb0a737111
Minhajur Rahman Chowdhury Mahim <86493750+minhaj1403@users.noreply.github.com>
2024-12-13 09:05:35 +0000

    ref fixed

--- config.py
+++ config.py
@@ -3,6 +3,11 @@ import torch
 
 # Command-line argument parser
 parser = argparse.ArgumentParser(description="Hyperparameter configuration")
+parser.add_argument('--n_layer', type=int, default=8, help="Number of layers (default: 12)")
+parser.add_argument('--n_head', type=int, default=8, help="Number of heads (default: 8)")
+parser.add_argument('--n_embd', type=int, default=384, help="Embedding size (default: 768)")
+parser.add_argument('--dropout', type=float, default=0.2, help="Dropout rate (default: 0.1)")
+parser.add_argument('--block_size', type=int, default=64, help="Block size (default: 64)")
 parser.add_argument('--batch_size', type=int, default=128, help="Batch size (default: 128)")
 parser.add_argument('--learning_rate', type=float, default=3e-4, help="Learning rate (default: 3e-4)")
 parser.add_argument('--max_iters', type=int, default=3000, help="Maximum iterations (default: 3000)")
@@ -13,6 +18,12 @@ parser.add_argument('--eval_interval', type=int, default=500, help="Evaluation i
 args = parser.parse_args()
 
 # Assign parsed values or defaults
+#n_layer, n_head, n_embd, dropout
+n_layer = args.n_layer
+n_head = args.n_head
+n_embd = args.n_embd
+dropout = args.dropout
+block_size = args.block_size
 batch_size = args.batch_size
 learning_rate = args.learning_rate
 max_iters = args.max_iters
